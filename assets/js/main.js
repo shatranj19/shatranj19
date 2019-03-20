af8dec5a130d79a4d51e31c3ca647b8e5ac74436
nilotpal2 <48655626+nilotpal2@users.noreply.github.com>
2019-03-20 19:20:32 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2019/04/04',function(event){
+     jQuery('#clock').countdown('2019/03/04',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
